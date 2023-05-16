78aa38f94bada2176c631e7473bfd3472e4529ed
RamyadeviS <118183330+RamyadeviS@users.noreply.github.com>
2023-05-16 10:43:40 +0530

    Add files via upload

---
+++ InvalidSourceException.java
@@ -0,0 +1,18 @@
+package com.chainsys.epassproject.myexception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InvalidSourceException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	static{
+
+	Logger logger=LoggerFactory.getLogger(InvalidSourceException.class);
+	logger.info("Invalid data!! Only allowed the alphabets");
+	}
+}
